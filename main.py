ac58a12893af580c3701d22863c5fa6c7889a1e3
bigwisdomdai <zhenghad@usc.edu>
2020-01-08 20:16:05 -0800

    inital

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
+import schedule_api as api
 
-
-# 下载网页
-url = "https://classes.usc.edu/term-20201/classes/csci/"
-response = requests.get(url)
-response.encoding = response.apparent_encoding
-
-# BeautifulSoup解析网页
-soup = BeautifulSoup(response.text, "html.parser")
-title = soup.find("div", {"id": "CSCI-102"})
-print(title.attrs)
-
-#print (soup.prettify())
+a = api.schedule_api()
+print(a.get_course_remain("csci-103", 29900, "Lecture"))
