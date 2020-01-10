724097a050a60fdc4e0314508abbe8f2b0a46e96
bigwisdomdai <zhenghad@usc.edu>
2020-01-10 00:46:34 -0800

    new fuc add

--- schedule_api.py
+++ schedule_api.py
@@ -52,7 +52,7 @@ class schedule_api:
                     for section in child.children:
 
                         if section.attrs['class'] == ['type']:
-                            if section.contents == [_type]:
+                            if section.string == [_type]:
                                 # if the section is colsed
                                 if child.contents[5].attrs['class'] == ['registered']:
                                     if child.contents[5].contents[1].name == 'div':
@@ -64,5 +64,46 @@ class schedule_api:
                                             remain[1])-int(remain[0])
                                     num_total += num_remain
         return num_total
+    
+    def get_list_course(self,course_id):
+        url = "https://classes.usc.edu/term-20201/course/"+str(course_id)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/51.0.2704.63 Safari/537.36'}
+
+        response = requests.get(url=url, headers=headers)
+        if response.status_code != 200:
+            raise Exception("404")
+        response.encoding = response.apparent_encoding
+        soup = BeautifulSoup(response.text, "html.parser")
+        test = soup.find("table", {"class": "sections responsive"})
+        num_total = 0
+        course_dict={}
+        for child in test.children:
+            # if not the header
+            if child.attrs['class'] != ['headers']:
+                
+                
+                for section in child.children:
+                    if section.attrs['class'] == ['type']:
+                        
+                        # if the section is colsed
+                        if child.contents[5].attrs['class'] == ['registered']:
+                            if child.contents[5].contents[1].name == 'div':
+                                num_remain = 0
+                                course_dict[child.attrs["data-section-id"]] = {
+                                    'type': section.string, 'remain': str(num_remain)}
+                            else:
+                                remain = child.contents[5].contents[1].split(
+                                    ' of ')
+                                num_remain = int(
+                                    remain[1])-int(remain[0])
+                                course_dict[child.attrs["data-section-id"]] = {
+                                    'type': section.string, 'remain': str(num_remain)}
+        
+        return course_dict
+
+
+
+    
 
 
